1e832e560f2ead9f0b28106eb16c3a6cf9ef6e23
Mikxus <91991837+Mikxus@users.noreply.github.com>
2022-08-21 01:59:22 +0300

    Renamed config and added new dynamic vector types

---
+++ src/config.h
@@ -0,0 +1,106 @@
+/* Config file to select wich interrupt vectors are binded at runtime */
+/* Uncomment to enable runtime binding for the interrupt */
+
+// #define INT0_used
+//
+// #define INT1_used
+//
+// #define PCINT0_used
+//
+// #define PCINT1_used
+//
+// #define PCINT2_used
+//
+// #define WDT_used
+//
+// #define TIMER2_COMPA_used
+//
+// #define TIMER2_COMPB_used
+//
+// #define TIMER2_OVF_used
+//
+// #define TIMER1_CAPT_used
+//
+// #define TIMER1_COMPA_used
+//
+// #define TIMER1_COMPB_used
+//
+// #define TIMER1_OVF_used
+//
+// #define TIMER0_COMPA_used
+//
+// #define TIMER0_COMPB_used
+//
+// #define TIMER0_OVF_used
+//
+// #define SPI_STC_used
+//
+// #define USART_RX_used
+//
+// #define USART_UDRE_used
+//
+// #define USART_TX_used
+//
+// #define ADC_used
+//
+// #define EE_READY_used
+//
+// #define ANALOG_COMP_used
+//
+// #define TWI_used
+//
+// #define SPM_READY_used 
+
+
+
+/* Unsafe dynamic interrupt vectors */
+
+// #define INT0_unsafe
+//
+// #define INT1_unsafe
+//
+// #define PCINT0_unsafe
+//
+// #define PCINT1_unsafe
+//
+// #define PCINT2_unsafe
+//
+// #define WDT_unsafe
+//
+// #define TIMER2_COMPA_unsafe
+//
+// #define TIMER2_COMPB_unsafe
+//
+// #define TIMER2_OVF_unsafe
+//
+// #define TIMER1_CAPT_unsafe
+//
+// #define TIMER1_COMPA_unsafe
+//
+// #define TIMER1_COMPB_unsafe
+//
+// #define TIMER1_OVF_unsafe
+//
+// #define TIMER0_COMPA_unsafe
+//
+// #define TIMER0_COMPB_unsafe
+//
+// #define TIMER0_OVF_unsafe
+//
+// #define SPI_STC_unsafe
+//
+// #define USART_RX_unsafe
+//
+// #define USART_UDRE_unsafe
+//
+// #define USART_TX_unsafe
+//
+// #define ADC_unsafe
+//
+// #define EE_READY_unsafe
+//
+// #define ANALOG_COMP_unsafe
+//
+// #define TWI_unsafe
+//
+// #define SPM_READY_unsafe 
