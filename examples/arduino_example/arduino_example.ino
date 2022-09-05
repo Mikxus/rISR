4039ed42524039f879fae8fa1b0c84294cf338cc
Mikxus <91991837+Mikxus@users.noreply.github.com>
2022-09-05 20:09:55 +0300

    Arduino example now works
    Removed unused files
    
    Added code to setup timer1

--- examples/arduino_example/arduino_example.ino
+++ examples/arduino_example/arduino_example.ino
@@ -1,8 +1,13 @@
-#define TIMER1_COMPA_used
 #include <rISR.h>
 
-#define led_pin 13
+/**
+ * @file Example runtime binding of interrupt.
+ * @note   To use the interrupt TIMER1_COMPB_ you need to uncomment TIMER1_COMPB_used in
+ *         /path to library/rISR/src/config.h
+ */
 
+
+#define led_pin 12
 volatile bool pin_state = 0;
 
 __attribute__ ((signal)) void __vector_example() 
@@ -18,8 +23,20 @@ void setup () {
 
 
 void loop () {
-  cli();
-  risr_bind( TIMER1_COMPA_, __vector_example );
+
+  Cli();
+  /* Sets timer1 to interrupt at the rate of ~ 16 mhz / 150 + interrupt execution time */
+  TCCR1A = 0;                            // set entire TCCR1A register to 0
+  TCCR1B = 0;                            // same for TCCR1B
+  TCNT1  = 0;                            // initialize counter value to 0 
+  PRR &= ~( 1 << PRTIM1 );               // Enable timer1
+
+  OCR1A = 150;
+  TCCR1B |= (1 << WGM12);                // turn on CTC mode
+  TCCR1B |= (1 << CS10);                 // No prescaler
+  TIMSK1 |= (1 << OCIE1B);               // Enable timer1  
+  
+  risr_bind( TIMER1_COMPB_, __vector_example );
   sei();
 
   while (true);
